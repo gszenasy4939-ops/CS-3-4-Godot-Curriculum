ba791eba6a845da1fb3cda4c8c8eaf3cf10c230e
gszenasy4939-ops <gszenasy4939@student.pps.net>
2025-09-11 14:05:31 -0700

    Added coin code

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,13 +1,26 @@
 # This next line allows the color of the coin to update without running the game
-# It can mostly be ignored, but it must be the first line of the script
 @tool
 
 extends Area2D
-
+class_name Coin
 
 @export var color: Color
+@export var gold_amount: int = 5
 
-
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	# Set the color of the coin
 	$AnimatedSprite2D.material.set_shader_parameter("color", color)
+	
+	
+func _ready():
+	print("coin created")
+	body_entered.connect(_on_body_entered)
+	
+	
+func _on_body_entered(body):
+	if body is Player:
+		print("Player found Coin!")
+		if body.has_method("collect_gold"):
+			body.gold(gold_amount)
+			print("Player collected " + str(gold_amount) + " Gold!")
+		queue_free()
